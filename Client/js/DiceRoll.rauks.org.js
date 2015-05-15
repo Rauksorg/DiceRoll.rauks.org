cfa59739408aaed4be1b6ec01ad2f59e74c4b776
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2015-05-15 23:57:42 +0000

    Remove Jquery

--- Client/js/DiceRoll.rauks.org.js
+++ Client/js/DiceRoll.rauks.org.js
@@ -2,26 +2,23 @@
 
 var DiceRoll = angular.module('DiceRoll', ['ngMaterial', 'ngMessages']);
 
-
 // New shake
 var myShakeEvent = new Shake({
-    threshold: 5,
-    timeout: 10
+  threshold: 5,
+  timeout: 10
 });
+
 // start listening to device motion
 myShakeEvent.start();
-
-
-
 
 // register a shake event
 DiceRoll.directive('shakeIt', function($window) {
   return {
     link: function(scope) {
-        //to test
-        scope.$broadcast('shakeIt::shake');
+      //to test
+      // scope.$broadcast('shakeIt::shake');
       angular.element($window).on('shake', function(e) {
-        
+
         scope.$broadcast('shakeIt::shake');
       });
     }
@@ -30,48 +27,29 @@ DiceRoll.directive('shakeIt', function($window) {
 
 
 DiceRoll.directive('myDirective', function() {
-    function randomIntFromInterval(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
+  function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
   return {
     link: function(scope, element) {
-        element.children().hide();
+      element.children().attr("hide", "true");
       scope.$on('shakeIt::shake', function() {
-          element.children().hide();
-          element.children().eq(randomIntFromInterval(0,element.children().length)-1).show();
+        element.children().attr("hide", "true");
+        element.children().eq(randomIntFromInterval(0, element.children().length) - 1).removeAttr("hide");
 
       });
     }
   };
 });
 
-// window.onload = function() {
-//     var timer;
 
-
-//     //create a new instance of shake.js.
-//     var myShakeEvent = new Shake({
-//         threshold: 5,
-//         timeout: 10
-//     });
-//     // start listening to device motion
-//     myShakeEvent.start();
-//     // register a shake event
-//     window.addEventListener('shake', shakeEventDidOccur, false);
-//     //shake event callback
-//     function shakeEventDidOccur() {
-//         clearInterval(timer);
-//         timer = setInterval(function() {
-//             result = 2;
-//             alert('Shake!');
-//             clearInterval(timer);
-//         }, 500);
-
-
-
-
-//         //put your own code here etc.
-
-//     }
-// };
+// // register a shake event
+// window.addEventListener('shake', shakeEventDidOccur, false);
+// //shake event callback
+// function shakeEventDidOccur(timer) {
+//   clearInterval(timer);
+//   timer = setInterval(function() {
+//     alert('Shake!');
+//     clearInterval(timer);
+//   }, 500);
+// }
