7f59cee3e4de1daf2946c5b40f8d8a14b40b8fb8
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2015-05-17 16:48:08 +0000

    Shaking icon and rolling circle

--- Client/js/DiceRoll.rauks.org.js
+++ Client/js/DiceRoll.rauks.org.js
@@ -11,13 +11,15 @@ var myShakeEvent = new Shake({
 // start listening to device motion
 myShakeEvent.start();
 
-// register a shake event
+// register a shake event on directive
 DiceRoll.directive('shakeIt', function($window) {
   var timer;
   return {
     link: function(scope) {
       //to test
-      // scope.$broadcast('shakeIt::shake');
+      // scope.$broadcast('shakeIt::shaked');
+      // scope.$broadcast('shakeIt::shaking');
+      // 
       angular.element($window).on('shake', function(e) {
         scope.$broadcast('shakeIt::shaking');
 
@@ -32,21 +34,28 @@ DiceRoll.directive('shakeIt', function($window) {
   };
 });
 
-DiceRoll.directive('myDirective', function() {
+DiceRoll.directive('myRandomshow', function() {
+  // Show a random tag from child tags
   function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
   return {
     link: function(scope, element) {
+      // initialy hide everything
       element.children().attr("hide", "true");
-      scope.$on('shakeIt::shaking', function() {
-        element.children().attr("hide", "true");
-      });
-      
-      scope.$on('shakeIt::shaked', function() {
-        element.children().attr("hide", "true");
-        element.children().eq(randomIntFromInterval(0, element.children().length) - 1).removeAttr("hide");
+      element.children().eq("0").removeAttr("hide");
 
+      scope.$on('shakeIt::shaking', function() {
+        // Hide everything while shaking
+        element.children().attr("hide", "true");
+        // Show dot while shaking
+        element.children().eq("1").removeAttr("hide");
+      });
+
+      scope.$on('shakeIt::shaked', function() {
+        // Show result
+        element.children().attr("hide", "true");
+        element.children().eq(randomIntFromInterval(2, element.children().length - 1)).removeAttr("hide");
       });
     }
   };
